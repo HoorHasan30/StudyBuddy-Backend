faae66b38a7b1300376359bfce9eaaa674fb4b19
WA-2211 <walaaalghanmi@gmail.com>
2026-08-07 17:31:24 +0300

    feat: add get TimeTable functionality

--- controllers/timeTable.controller.js
+++ controllers/timeTable.controller.js
@@ -1,21 +1,41 @@
+const { json } = require('express')
 const TimeTable = require('../models/TimeTable')
 
-async function createTableImg(req, res){
+async function createTableImg(req, res) {
     try {
-        const {tableImage} = req.body
+        const { tableImage } = req.body
         const createdImg = await TimeTable.create({
             tableImage,
             owner: req.user._id
         })
         res.status(201).json(createdImg)
-        
+
     } catch (error) {
         res.status(500).json({ message: error.message })
 
     }
 }
-async function getTableImg(req, res){}
-async function deleteTableImg(req, res){}
+async function getTableImg(req, res) {
+
+    try {
+        const foundTimeTable = await TimeTable.findOne({ owner: req.user._id })
+
+        if (!foundTimeTable) {
+            res.status(404).json({ message: 'no timeTable found, add yours!' })
+
+        }
+        res.status(200).json(foundTimeTable)
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+
+    }
+}
+
+async function deleteTableImg(req, res) {
+
+
+}
 
 module.exports = {
     createTableImg,
--- routes/timeTable.routes.js
+++ routes/timeTable.routes.js
@@ -3,5 +3,6 @@ const timetableController = require('../controllers/timeTable.controller')
 const verifyToken = require('../middleware/verifyToken')
 
 router.post('/', verifyToken, timetableController.createTableImg)
+router.get('/', verifyToken, timetableController.getTableImg)
 
 module.exports = router
