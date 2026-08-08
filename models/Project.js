bcbe73b33f00023673a2150a4e4db4dad742da93
HoorHasan30 <202301820@student.polytechnic.bh>
2026-08-08 17:43:17 +0300

    Feat: Add Project Model

--- models/Project.js
+++ models/Project.js
@@ -1,2 +1,30 @@
 const mongoose = require('mongoose')
 
+const projectSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: True
+    },
+    description: {
+        type: String
+    },
+    deadline: {
+        type: Date
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    collaberators: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    tasks: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Task'
+    }]
+})
+
+const Project = mongoose.model('Project', projectSchema)
+
+module.exporte = Project
