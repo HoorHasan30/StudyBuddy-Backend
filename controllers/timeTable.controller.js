f889783ed0171a59ae01baa96f4e959ef03acfd6
WA-2211 <walaaalghanmi@gmail.com>
2026-08-08 12:21:35 +0300

    feat: add delete TimeTable functionality & update create TimeTable

--- controllers/timeTable.controller.js
+++ controllers/timeTable.controller.js
@@ -1,13 +1,13 @@
-const { json } = require('express')
 const TimeTable = require('../models/TimeTable')
 
 async function createTableImg(req, res) {
     try {
         const { tableImage } = req.body
-        const createdImg = await TimeTable.create({
+        const createdImg = await TimeTable.findOneAndUpdate({owner:req.user._id},
+            {
             tableImage,
             owner: req.user._id
-        })
+        }, {new: true, upsert:true})
         res.status(201).json(createdImg)
 
     } catch (error) {
@@ -21,7 +21,7 @@ async function getTableImg(req, res) {
         const foundTimeTable = await TimeTable.findOne({ owner: req.user._id })
 
         if (!foundTimeTable) {
-            res.status(404).json({ message: 'no timeTable found, add yours!' })
+            return res.status(404).json({ message: 'no timeTable found, add yours!' })
 
         }
         res.status(200).json(foundTimeTable)
@@ -34,7 +34,18 @@ async function getTableImg(req, res) {
 
 async function deleteTableImg(req, res) {
 
+    try {
+        const deletedTimeTable = await TimeTable.findOneAndDelete({owner: req.user._id})
+        if(!deleteTableImg){
+            return res.status(404).json({ message: 'no timeTable found, add yours!' })
 
+        }
+        res.status(204).json(deletedTimeTable)
+        
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+
+    }
 }
 
 module.exports = {
--- routes/timeTable.routes.js
+++ routes/timeTable.routes.js
@@ -4,5 +4,7 @@ const verifyToken = require('../middleware/verifyToken')
 
 router.post('/', verifyToken, timetableController.createTableImg)
 router.get('/', verifyToken, timetableController.getTableImg)
+router.delete('/', verifyToken, timetableController.deleteTableImg)
+
 
 module.exports = router
