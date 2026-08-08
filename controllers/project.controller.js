b69adea1364e0ca5e14649ddd7263917a3ddef3f
HoorHasan30 <202301820@student.polytechnic.bh>
2026-08-08 18:09:34 +0300

    Feat: Add Create project & Get my projects routes

--- controllers/project.controller.js
+++ controllers/project.controller.js
@@ -24,6 +24,18 @@ async function createProject(req, res){
     }
 }
 
+// Get my all projects
+async function getMyProjects(req, res){
+    try{
+        const myProjects = await Project.find({owner: req.user._id})
+        res.status(200).json(myProjects)
+    }
+    catch(err){
+        return res.status(500).json({message: err.message})
+    }
+}
+
 module.exports = {
-    createProject
+    createProject,
+    getMyProjects
 }
--- routes/project.routes.js
+++ routes/project.routes.js
@@ -5,5 +5,6 @@ const verifyToken = require("../middleware/verifyToken");
 const validateObjectId = require('../middleware/validateObjectId')
 
 router.post('/', verifyToken, projectController.createProject)
+router.get('/', verifyToken, projectController.getMyProjects)
 
 module.exports = router;
