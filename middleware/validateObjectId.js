2aa634ee29e49dc88a40025fbda61dad3a801c05
HoorHasan30 <202301820@student.polytechnic.bh>
2026-08-09 09:55:53 +0300

    Update: update the validateObject middleware to validate all obj

--- middleware/validateObjectId.js
+++ middleware/validateObjectId.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose')
-function validateObjectId(req,res,next){
-if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-      return res.status(404).json({ message: "No object matching id provided" });
-    }
-next()
+function validateObjectId(req, res, next) {
+  // Checking the project
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(404).json({ message: "No object matching id provided" });
+  }
+
+  // checking the course
+  if (!mongoose.Types.ObjectId.isValid(req.params.courseId)) {
+    return res.status(404).json({ message: "No object matching id provided" });
+  }
+  // checking the task
+  if (!mongoose.Types.ObjectId.isValid(req.params.taskId)) {
+    return res.status(404).json({ message: "No object matching id provided" });
+  }
+
+  next()
 }
 
 module.exports = validateObjectId
