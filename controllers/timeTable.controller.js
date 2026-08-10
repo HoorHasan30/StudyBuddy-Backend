b0b103eff50fc882ff8c15f691784fe6046044f3
WA-2211 <walaaalghanmi@gmail.com>
2026-08-10 18:52:32 +0300

    feat: add craete timetable - cloudinary and multer

--- controllers/timeTable.controller.js
+++ controllers/timeTable.controller.js
@@ -1,15 +1,36 @@
 const TimeTable = require('../models/TimeTable')
+const cloudinary = require('../middleware/cloudinary')
+const { promises, Stream } = require('supertest/lib/test')
+
+const uploadImage =  (fileBuffer)=>{
+    return new Promise((resolve, reject) =>{
+
+        const uploadStream = cloudinary.uploader.upload_stream(
+            {
+                folder: 'timetable',
+                resource_type: 'image'
+            }, (error, result)=>{
+            if (error) return reject(error)
+                resolve(result)
+        })
+        uploadStream.end(fileBuffer)
+    })
+}
 
 async function createTableImg(req, res) {
     try {
-        const { tableImage } = req.body
+        if(!req.file){
+            return res.status(400).json({message: 'no timetable found'})
+        }
 
+        const result = await uploadImage(req.file.buffer)  
         const createdImg = await TimeTable.findOneAndUpdate({owner:req.user._id},
             {
-            tableImage,
+            tableImage: {url: result.secure_url},
             owner: req.user._id
         }, {new: true, upsert:true})
 
+        
         res.status(201).json(createdImg)
 
     } catch (error) {
@@ -30,7 +51,7 @@ async function getTableImg(req, res) {
 
     } catch (error) {
         if (err.name === 'ValidationError') {
-            return res.status(400).json({ message: err.message })
+            return res.status(400).json({ message: error.message })
         }
         
         res.status(500).json({ message: error.message })
@@ -43,7 +64,7 @@ async function deleteTableImg(req, res) {
     try {
         const deletedTimeTable = await TimeTable.findOneAndDelete({owner: req.user._id})
         
-        if(!deleteTableImg){
+        if(!deletedTimeTable){
             return res.status(404).json({ message: 'no timeTable found, add yours!' })
 
         }
