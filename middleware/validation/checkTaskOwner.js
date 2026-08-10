de83225c7107f204774811e68085f4d3033a31bc
HoorHasan30 <202301820@student.polytechnic.bh>
2026-08-10 13:41:48 +0300

    Fix: Fix checkTaskOwner Middleware

--- middleware/validation/checkTaskOwner.js
+++ middleware/validation/checkTaskOwner.js
@@ -3,11 +3,16 @@ const Task = require('../../models/Task')
 async function checkTaskOwner(req, res, next) {
 
     const foundTask = await Task.findById(req.params.taskId).populate('owner')
+    
+    const isOwner = foundTask.owner?._id
+        ? foundTask.owner._id.toString() === req.user._id.toString()
+        : foundTask.owner?.toString() === req.user._id.toString()
 
-    if (foundTask.owner != req.user._id) {
+
+    if (!isOwner) {
         return res.status(403).json({ message: 'You are not authorized to do this action' })
     }
-    
+
     req.foundTask = foundTask
     next()
 }
