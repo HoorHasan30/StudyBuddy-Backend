6af8cb690d63f4f5223a95e4162c95088d164ed5
HoorHasan30 <202301820@student.polytechnic.bh>
2026-08-13 07:12:52 +0300

    Update checkProjectOwner.js

--- middleware/validation/checkProjectOwner.js
+++ middleware/validation/checkProjectOwner.js
@@ -9,7 +9,6 @@ async function checkProjectOwner(req, res, next) {
             select: '_id username'
         }
     }).populate('collaberators')
-    .populate({ path: 'owner', select: '_id username' })
 
     const isOwner = foundProject.owner.toString() === req.user._id.toString()
 
